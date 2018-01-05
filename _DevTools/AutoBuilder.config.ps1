fd9e00e2e01e81b269e16a5f6fd919254a279b81
Davide Gironi <davide.gironi@gmail.com>
2018-01-05 18:24:25 +0100

    moved to .net 4 for retro compatibility

--- _DevTools/AutoBuilder.config.ps1
+++ _DevTools/AutoBuilder.config.ps1
@@ -6,7 +6,7 @@ $solutionName = "EnhancedBoxHelpers"
 $versionMajor = "1"
 $versionMinor = "0"
 $versionBuild = GetVersionBuild
-$versionRevision = "10"
+$versionRevision = "11"
 #build version number
 $assemblyVersion = GetVersion $versionMajor $versionMinor $versionBuild $versionRevision
 $fileVersion = $assemblyVersion
