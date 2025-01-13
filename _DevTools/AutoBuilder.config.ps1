8e6166297bacb4b4a5a8f8a3bec6052fb28f890e
Davide Gironi <davide.gironi@gmail.com>
2025-01-13 19:52:09 +0100

    added net6.0-windows net7.0-windows net8.0-windows, updated version

--- _DevTools/AutoBuilder.config.ps1
+++ _DevTools/AutoBuilder.config.ps1
@@ -6,7 +6,7 @@ $solutionName = "EnhancedBoxHelpers"
 $versionMajor = "1"
 $versionMinor = "0"
 $versionBuild = GetVersionBuild
-$versionRevision = "19"
+$versionRevision = "20"
 #build version number
 $version = GetVersion $versionMajor $versionMinor $versionBuild $versionRevision
 
